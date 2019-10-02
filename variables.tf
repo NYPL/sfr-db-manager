ce9421331d48193a7595d27f4d7bb7cc279e9a1f
Mike Benowitz <mwbenowitz@gmail.com>
2019-10-02 10:41:31 -0400

    Add Additional variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
 variable "out_file" {
     default = "builtSource.zip"
 }
+
+variable "environment" {
+    default = "development"
+}
+
+variable "db_port" {
+    default = 5432
+}
+
+variable "log_level" {
+    default = "info"
+}
+
+variable "db_host_encoded" {}
